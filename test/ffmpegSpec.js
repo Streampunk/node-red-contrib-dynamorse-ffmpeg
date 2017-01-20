eb786d78fc388be8b4aa46864364b91050aba9dc
Simon Rogers <scriptorian@streampunk.media>
2017-01-20 19:15:08 +0000

    add first test support

---
+++ test/ffmpegSpec.js
@@ -0,0 +1,84 @@
+/* Copyright 2016 Streampunk Media Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+var TestUtil = require('dynamorse-test').TestUtil;
+
+var encodeTestNode = JSON.stringify({
+  "type": "encoder",
+  "z": TestUtil.testFlowId,
+  "name": "encode-test",
+  "maxBuffer": 10,
+  "wsPort": TestUtil.properties.wsPort,
+  "x": 300.0,
+  "y": 100.0,
+  "wires": [[]]
+});
+
+var decodeTestNode = JSON.stringify({
+  "type": "decoder",
+  "z": TestUtil.testFlowId,
+  "name": "decoder-test",
+  "x": 500.0,
+  "y": 100.0,
+  "wires": [[]]
+});
+
+var funnelNodeId = "24fde3d7.b7544c";
+var encoderNodeId = "7c968c40.836974";
+var decoderNodeId = "634c3672.78be18";
+var spoutNodeId = "f2186999.7e5f78";
+
+TestUtil.nodeRedTest('A src->encoder->decoder->spout flow is posted to Node-RED', {
+  numPushes: 10,
+  funnelMaxBuffer: 10,
+  encodeFmt: 'h264',
+  encoderMaxBuffer: 10,
+  decoderMaxBuffer: 10,
+  spoutTimeout: 0
+}, function getFlow(params) {
+  var testFlow = JSON.parse(TestUtil.testNodes.baseTestFlow);
+  testFlow.nodes[0] = JSON.parse(TestUtil.testNodes.funnelGrainNode);
+  testFlow.nodes[0].id = funnelNodeId;
+  testFlow.nodes[0].numPushes = params.numPushes;
+  testFlow.nodes[0].maxBuffer = params.funnelMaxBuffer;
+  testFlow.nodes[0].wires[0][0] = encoderNodeId;
+
+  testFlow.nodes[1] = JSON.parse(encodeTestNode);
+  testFlow.nodes[1].id = encoderNodeId;
+  testFlow.nodes[1].dstFormat = params.encodeFmt;
+  testFlow.nodes[1].maxBuffer = params.encoderMaxBuffer;
+  testFlow.nodes[1].wires[0][0] = decoderNodeId;
+
+  testFlow.nodes[2] = JSON.parse(decodeTestNode);
+  testFlow.nodes[2].id = decoderNodeId;
+  testFlow.nodes[2].maxBuffer = params.decoderMaxBuffer;
+  testFlow.nodes[2].wires[0][0] = spoutNodeId;
+
+  testFlow.nodes[3] = JSON.parse(TestUtil.testNodes.spoutTestNode);
+  testFlow.nodes[3].id = spoutNodeId;
+  testFlow.nodes[3].timeout = params.spoutTimeout;
+  testFlow.nodes[3].x = 700.0;
+  return testFlow;
+}, function onMsg(t, params, msgObj, onEnd) {
+  //t.comment(`Message: ${JSON.stringify(msgObj)}`);
+  if (msgObj.hasOwnProperty('receive')) {
+    TestUtil.checkGrain(t, msgObj.receive);
+    params.count++;    
+  }
+  else if (msgObj.hasOwnProperty('end') && (msgObj.src === 'spout')) {
+    t.equal(params.count, params.numPushes, `received end after expected number of pushes`);
+    onEnd();
+  }
+});
